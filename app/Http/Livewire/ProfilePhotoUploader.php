55788815d8bf759bf3ee1b6344dde8d51f697ed2
Jander Oliveira - PC <janderfrancisco@hotmail.com>
2021-03-16 11:41:18 -0300

    first commit

---
+++ app/Http/Livewire/ProfilePhotoUploader.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use App\Events\ProfilePhotoUpdated;
+
+class ProfilePhotoUploader extends Component
+{
+    use WithFileUploads;
+    
+    public $photo;
+
+    protected $rules = [
+        'photo' => 'image|max:1024'
+    ];
+
+    public function updatedPhoto()
+    {
+        $this->resetErrorBag();
+    }
+
+    public function storePhoto()
+    {
+        $this->validate();
+
+        auth()->user()->update([
+            'profile_photo' => $this->photo->store('profile-photos', 'public')
+        ]);
+
+        $this->photo = null;
+
+        broadcast(new ProfilePhotoUpdated(auth()->user()));
+    }
+
+    public function render()
+    {
+        return view('livewire.profile-photo-uploader');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'profile_photo'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function profilePhoto()
+    {
+        if (!$this->profile_photo) {
+            return $this->defaultProfilePhotoUrl();
+        }
+
+        return Storage::disk('public')->url($this->profile_photo);
+    }
+
+    protected function defaultProfilePhotoUrl()
+    {
+        return 'https://ui-avatars.com/api/?name=' . urlencode($this->name);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
